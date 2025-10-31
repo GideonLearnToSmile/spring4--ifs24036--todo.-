8c8e374d4f5aac2285140f75ff8ae94019e1ea38
GideonLearnToSmile <deonpanjaitan010@gmail.com>
2025-10-31 22:17:51 +0700

    tambahan

--- src/main/java/org/delcom/starter/controllers/MatriksController.java
+++ src/main/java/org/delcom/starter/controllers/MatriksController.java
@@ -1,3 +1,4 @@
+package org.delcom.starter.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/org/delcom/starter/controllers/NIMController.java
+++ src/main/java/org/delcom/starter/controllers/NIMController.java
@@ -1,3 +1,4 @@
+package org.delcom.starter.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
